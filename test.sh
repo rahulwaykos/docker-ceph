a91e698c7614be0c8bc617c7f02380b7f98fc823
rahulwaykos <60727515+rahulwaykos@users.noreply.github.com>
2020-04-28 22:34:39 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
 
 
-kubectl patch storageclass rook-ceph-block -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+#kubectl patch storageclass rook-ceph-block -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+
+
+kubectl exec -n rook-ceph -it $(kubectl get -n rook-ceph pod -l "app=rook-ceph-tools" -o jsonpath='{.items[0].metadata.name}') bash
